385b4ab6d3be4029f5226b6dc43562516439339d
AlinaChumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2024-04-08 22:04:25 +0300

    move

--- js/task-01.js
+++ js/task-01.js
@@ -9,37 +9,32 @@ function onClick() {
   outputEl.textContent = inputEl.value;
 }
 
-// function onMouseDown(e) {
-//   console.log("event mousedown", e);
-//   const drag = e.target;
+function onMouseDown(e) {
+  const drag = e.target;
 
-//   drag.style.position = "absolute";
-//   drag.style.zIndex = 1000;
+  drag.style.position = "absolute";
+  drag.style.zIndex = 1000;
 
-//   document.body.append(drag);
+  document.body.append(drag);
 
-//   function moveDrag(pageX, pageY) {
-//     drag.style.left = pageX - drag.offsetWidth / 2 + "px";
-//     drag.style.top = pageY - drag.offsetHeight / 2 + "px";
-//   }
+  function moveDrag(pageX, pageY) {
+    drag.style.left = pageX - drag.offsetWidth / 2 + "px";
+    drag.style.top = pageY - drag.offsetHeight / 2 + "px";
+  }
 
-//   moveDrag(e.pageX, e.pageY);
+  moveDrag(e.pageX, e.pageY);
 
-//   function onMouseMove(e) {
-//     moveDrag(e.pageX, e.pageY);
-//   }
+  function onMouseMove(e) {
+    moveDrag(e.pageX, e.pageY);
+  }
 
-//   document.addEventListener("mousemove", onMouseMove);
+  document.addEventListener("mousemove", onMouseMove);
 
-//   // Обробник події 'mouseup' видаляє обробник 'mousemove' після відпускання миші
-//   drag.onmouseup = function () {
-//     document.removeEventListener("mousemove", onMouseMove);
-//     drag.onmouseup = null;
-//   };
-// }
-
-// // Забороняємо стандартну поведінку перетягування
-
-// drag.ondragstart = function () {
-//   return false;
-// };
+  drag.onmouseup = function () {
+    document.removeEventListener("mousemove", onMouseMove);
+    drag.onmouseup = null;
+  };
+}
+drag.ondragstart = function () {
+  return false;
+};
